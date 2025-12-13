3ecf37a9e5826826014f25ffbd636ab81731245f
Abhishek.k <abhiabhishekk994@gmail.com>
2025-12-13 23:01:52 +0530

    first commit

---
+++ object_class.py
@@ -0,0 +1,10 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print(self.name, self.age)
+
+s1 = Student("Abhi", 21)
+s1.display()
